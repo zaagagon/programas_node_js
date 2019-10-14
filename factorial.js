587d7b58f44b9b9bae92f387c356df658eab4e1e
zag <zaagagon@gmail.com>
2019-10-14 05:11:45 -0500

    cambios a funciones

--- factorial.js
+++ factorial.js
@@ -3,10 +3,12 @@
 //usemos una funcion para este ejemplo
 function factorial(n){
     var a =1;
-    
-    for( i=1;i<=n;i++){
-        a=a*i;
 
-    }
-    return a;
+    for( i=1;i<=n;i++){
+       var  a=a*i;
+       
+return a;
 }
+}
+console.log('dfadj'+ a);
+factorial(4);
---
+++ funciones.js
@@ -0,0 +1,11 @@
+/* 
+archivo: ejemplos de funciones
+author : @zag
+*/
+
+//funcion nombre
+function nombre (name){
+    return name;
+}
+//enviamos el argumento zag a la funcion
+console.log('Mi nombre es ' + nombre('zag'));
