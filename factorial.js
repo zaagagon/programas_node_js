db37e131962c6774bad19501f4690fdaadf79e85
zag <zaagagon@gmail.com>
2019-10-14 04:03:25 -0500

    archivos factorial y suma

---
+++ factorial.js
@@ -0,0 +1,12 @@
+//repasemos un poco factorial
+//4!: 4*3*2*1 = ?
+//usemos una funcion para este ejemplo
+function factorial(n){
+    var a =1;
+    
+    for( i=1;i<=n;i++){
+        a=a*i;
+
+    }
+    return a;
+}
---
+++ suma.js
@@ -0,0 +1,16 @@
+//operacion suma
+console.log("hola error");
+var a=2,b=2, c;
+
+function suma(a,b){
+   return  a + b;
+    //return c;
+
+};
+//la funcion suma espera 2 parametros
+//sino los ubicamos no imprime el resultado
+//datos por variale
+console.log('el resultado es ..'+ suma(a,b));
+//otro ejemplo es con datos quemados en la misma funcion
+console.log('el resultado es ...'+suma (45,90));
+
