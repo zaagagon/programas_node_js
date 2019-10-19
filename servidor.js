eebc43b85527a71a83944500e73307454103803f
Zag <zaagagon@gmail.com>
2019-10-19 02:17:30 -0500

    probando ejemplo de callback

--- servidor.js
+++ servidor.js
@@ -1,2 +1,15 @@
 //hacemos uso del modulo http
+/*
+variable http que en realidad es un objeto, 
+sobre el que podemos invocar 
+métodos que estaban en el módulo requerido
+*/ 
 var http = require("http");
+
+//ejemplo de un callback
+
+function callback() {
+    return '**El mundo de zag**';
+}
+
+console.log(callback());
