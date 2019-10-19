3194aff339ecc1eaa371979ea423dc14a9f9402f
Zag <zaagagon@gmail.com>
2019-10-19 03:35:07 -0500

    cambios en servidor y callback

--- callback.js
+++ callback.js
@@ -3,3 +3,13 @@ function callback(){
     return 'este es un ejemplo';
 }
  console.log(callback());
+
+
+
+ function hagoAlgo(callback) {
+    callback();
+}
+
+hagoAlgo(function(){
+    console.log('Hola Zaagagon !!');
+})
--- servidor.js
+++ servidor.js
@@ -5,7 +5,14 @@ sobre el que podemos invocar
 métodos que estaban en el módulo requerido
 */ 
 var http = require("http");
+//creacion del servidor usando el puerto 3000
+var servidor = http.createServer(function(peticion,respuesta){
+respuesta.end("hola mundo de zag.com");
+});
 
+servidor.listen(3000,function(){
+   console.log("Servidor escuchando y listo"+ this.address().port); 
+});
 //ejemplo de un callback
 
 function callback() {
