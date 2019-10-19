beff7b5394d6f30084b93508746132bb73948272
Zag <zaagagon@gmail.com>
2019-10-18 19:21:56 -0500

    cambios luego de recuperados de la nube

--- funciones.js
+++ funciones.js
@@ -3,25 +3,57 @@ archivo: ejemplos de funciones
 author : @zag
 */
 
-//funcion nombre
+
+//sintaxis de una funcion 
+
+/*function nombre_funcion(parametros){
+
+    instrucciones;
+  }*/
+
+  //funcion nombre
+  var name2;
 function nombre (name){
     return name;
 }
 //enviamos el argumento zag a la funcion
 console.log('Mi nombre es ' + nombre('zag'));
+name2 ="alirio"
+console.log(name2);
+console.log('mi nombres es ..'+ name2); 
 
 //funcion n2
-
-function saludar(){
+//var b;
+/*function saludar(){
 
     //console.log();
-    alert("hola zag ");
-} saludar();
+ alert("hola zag");
+} 
+// posible error llamar asi la funcion saludar();
+console.log(saludar());*/
 
-function factorialR(){
+function factorialR(n){
     if(n==0){
         return 1
     } else {
         return factorialR(n-1)*n
     }
 }
+//mostremos  en pantalla la funcion factorailR
+
+console.log(factorialR(5));  
+(function (global) {
+
+    "use strict";
+    
+    var alert;  //  added this in to fix
+    
+    function initialize_page()
+      {
+      global.alert ("hi");
+      }
+    
+    addEventListener('load', initialize_page);
+    
+    });
+   //s (window);
