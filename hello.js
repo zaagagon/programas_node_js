5eed53d6552120a35faa425fa66abd46d0daf7b0
zaagagon <zaagagon@gmail.com>
2023-10-14 01:47:09 -0500

    hola argv console clear

--- hello.js
+++ hello.js
@@ -1,3 +1,12 @@
+
+console.clear() //limpia pantalla
 //Ejemplo de hello zag
 console.log("EL MUNDO DE ZAG");
 console.log('hola zag');
+
+//PRUEBA CON  argv
+
+console.log(process.argv);
+
+//tomar el nombre por consola
+console.log("Hola " +process.argv[2]);
