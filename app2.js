0bdf0581cbe08ee8a1a4d1b57d038d634f9a6327
zaagagon <zaagagon@gmail.com>
2023-10-14 23:28:22 -0500

    comentarios app2

--- app2.js
+++ app2.js
@@ -1,9 +1,19 @@
+//importamos el modudlo
 const express=require('express')
 
+//creamos el objeto
 const app2= express()
 
-app2.get('/',function(req,res))
+app2.get('/',function(req,res){
+
+    res.send("hola user")
+}
+//res.send("hola user")
+
+
+)
 
 //puerto del servidor
+//usamos el metodo listen
 app2.listen(3004)
 console.log("servidor en linea esperando ...");
