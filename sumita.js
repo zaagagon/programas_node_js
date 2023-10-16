fd897937dc009dc0c745e306d0ea3ac18cb52ea1
zaagagon <zaagagon@gmail.com>
2023-10-16 00:35:55 -0500

    confirmación okk chalk version 4.1.2

--- sumita.js
+++ sumita.js
@@ -3,3 +3,11 @@
 const chalk = require('chalk')
 console.log(chalk.blue("hola"));
 //npm uninstall chalk
+
+//reparamos con la version
+//npm i chalk@4.1.2
+
+//Ejemplo 
+
+
+console.log(chalk.red('Hello world chalk!')); 
