ba401c8d7d56feb0bb5a39f777450da8677d2418
zaagagon <zaagagon@gmail.com>
2023-10-17 00:15:24 -0500

    operaciones para consultar arreglo

--- objeto1.js
+++ objeto1.js
@@ -15,5 +15,8 @@ let heroes =[
 ]
 //muestra tamaño del objeto
 console.log(heroes.length);
-//
+//muestra contenido de array de objetos
 console.log(heroes);
+
+//muestra la información dentro del arreglo[1]=sapo
+console.log(heroes[1]);
