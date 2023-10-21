c57296270727c55773623bf7abcabd3aae79a885
zaagagon <zaagagon@gmail.com>
2023-10-21 00:28:18 -0500

    __dirname con log

--- path.js
+++ path.js
@@ -1,8 +1,11 @@
 const { log } = require('console')
 const path = require('path')
 
-console.log(path);
+//console.log(path);
+//console.log(path.dirname);
 
 //mostrar separador 
 
-//console.log(`El separador en mi Sistema Operativo es: ${path.sep}`)
+console.log(`El separador en mi Sistema Operativo es: ${path.sep}`)
+//muestra directorio actual
+console.log(__dirname);
