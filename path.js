a07999e35b1271c82a65b3919b79b216f41a72c9
zaagagon <zaagagon@gmail.com>
2023-10-21 00:10:44 -0500

    modulo path en nodejs

---
+++ path.js
@@ -0,0 +1,8 @@
+const { log } = require('console')
+const path = require('path')
+
+console.log(path);
+
+//mostrar separador 
+
+//console.log(`El separador en mi Sistema Operativo es: ${path.sep}`)
