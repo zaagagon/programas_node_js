c61cf527dbb742e465a99627fcc57aa746af16f7
zaagagon <zaagagon@gmail.com>
2023-10-21 01:14:18 -0500

    servidor de archivos estaticos

--- servidor_web/index.js
+++ servidor_web/index.js
@@ -4,7 +4,10 @@ const express = require('express')
 //creamos objeto o instancia de express
 const app = express()
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public/'))
+//app.use(express.static(__dirname + '/public/'));
+console.log(__dirname + '/public');
 
-app.listen(3005)
+
+app.listen(3006)
 console.log('servidor web en puerto ');
