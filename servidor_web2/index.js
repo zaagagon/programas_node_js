54768576af163938f334d27d582a9a1201c46e1f
zaagagon <zaagagon@gmail.com>
2023-10-21 01:44:25 -0500

    servidor2 corriendo

---
+++ servidor_web2/index.js
@@ -0,0 +1,9 @@
+//importamos express
+
+const express =require('express')
+//creamos objeto o instancia de express
+
+const app = express()
+
+app.listen(3008)
+console.log("servidor 2 escuchando ");
