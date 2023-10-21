bda10f9632013aeccaa73f9808d4d0bbeea984ea
zaagagon <zaagagon@gmail.com>
2023-10-21 02:03:15 -0500

    ruta / servidor2

--- servidor_web2/index.js
+++ servidor_web2/index.js
@@ -8,6 +8,11 @@ const app = express()
 //puerto del servidor
 const puerto=3008
 
+//ruta /
+app.get('/',(req,res)=>{
+    res.send('<h1>Pagina en construcción</h1>')
+})
+
 app.listen(puerto)
 console.log("servidor 2 escuchando " + puerto);
 //cadena con variable puerto
