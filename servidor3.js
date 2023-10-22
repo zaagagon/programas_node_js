e3c152bbc9ac3d23e030eab6388edbe28d24ef5d
zaagagon <zaagagon@gmail.com>
2023-10-22 01:29:41 -0500

    rutas y variable entorno servidor3

---
+++ servidor3.js
@@ -0,0 +1,24 @@
+var express = require('express') //llamamos a Express
+var app = express()               
+
+var port = process.env.PORT || 3005  // establecemos nuestro puerto
+
+app.get('/', function(req, res) {
+  res.json({ mensaje: '¡Hola Mundo!' })   
+})
+
+app.get('/cervezas', function(req, res) {
+  res.json({ mensaje: '¡A beber cerveza!' })  
+})
+
+app.post('/', function(req, res) {
+  res.json({ mensaje: 'Método post' })   
+})
+
+app.del('/', function(req, res) {
+  res.json({ mensaje: 'Método delete' })  
+})
+
+// iniciamos nuestro servidor
+app.listen(port)
+console.log('API escuchando en el puerto ' + port)
