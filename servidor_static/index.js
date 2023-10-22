731d8e399462f9ce807046592d236f3b2b663cea
zaagagon <zaagagon@gmail.com>
2023-10-22 12:09:56 -0500

    express instalado

--- servidor_static/index.js
+++ servidor_static/index.js
@@ -1,3 +1,9 @@
+//instalamos express
+
+//podriamos crear nuestro json con npm init -y 
+//crea por defecto el json
+
+//npm i express -- instala node_modules
 
 
 console.log("servidor estatico construcción");
