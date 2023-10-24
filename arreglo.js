2bc04669eae99ae970bf35d00cde67988609c1ed
zaagagon <zaagagon@gmail.com>
2023-10-24 09:39:10 -0500

    importamos chalk

--- arreglo.js
+++ arreglo.js
@@ -1,7 +1,6 @@
 const chalk = require("chalk");
 
 
-const format=chalk()
 
 const numeros=[3,4,5,5]
 
@@ -10,5 +9,6 @@ const numeros=[3,4,5,5]
 console.log("programa para arreglo");
 console.log(numeros[0]);
 
-//metodos con arreglos
-console.log((numeros.length));
+//metodos con arreglos 
+//format color de salida
+console.log(chalk.red(numeros.length));
