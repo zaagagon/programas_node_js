23a79fcafaab117b2b79546718473af0427d7750
zaagagon <zaagagon@gmail.com>
2023-10-24 09:36:12 -0500

    length en arreglos js

--- arreglo.js
+++ arreglo.js
@@ -1,6 +1,14 @@
-const numeros=[3,4,5]
+const chalk = require("chalk");
+
+
+const format=chalk()
+
+const numeros=[3,4,5,5]
 
 //mostrar arreglo en posicio 0
 
 console.log("programa para arreglo");
 console.log(numeros[0]);
+
+//metodos con arreglos
+console.log((numeros.length));
