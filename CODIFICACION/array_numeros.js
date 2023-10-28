f575b90cafd50741c432c8cc683db934337f1e53
zaagagon <zaagagon@gmail.com>
2023-10-27 22:22:03 -0500

    recorrido de array con foreach

--- CODIFICACION/array_numeros.js
+++ CODIFICACION/array_numeros.js
@@ -4,13 +4,18 @@ const numeros=[4,6,5,8,9]
 
 //mostrar tamaño del arreglo
 console.log("tamaño del arreglo :" +numeros.length);
-//mostremos el arreglo
-
 
 //sumemos los elementos del arreglo
+
 //funcion flecha
 const sumita=numeros.reduce((acumulador,numero)=>{
     return acumulador + numero
 },0)
 //mostramos sumita
 console.log(`resultado de suma de elementos ${sumita}`);
+//mostremos el arreglo
+console.log("elementos del arreglo");
+numeros.forEach((numero) => {
+    console.log(numero);
+    
+});
