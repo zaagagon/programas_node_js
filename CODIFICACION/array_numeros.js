34bffb3a09b9610851c2050958ce9792e6e16f14
zaagagon <zaagagon@gmail.com>
2023-10-27 22:17:07 -0500

    reduce funcion flecha

--- CODIFICACION/array_numeros.js
+++ CODIFICACION/array_numeros.js
@@ -4,4 +4,11 @@ const numeros=[4,6,5,8,9]
 
 //mostrar tamaño del arreglo
 console.log("tamaño del arreglo :" +numeros.length);
+//mostremos el arreglo
 
+
+//sumemos los elementos del arreglo
+//funcion flecha
+numeros.reduce((acumulador,numero)=>{
+    return acumulador + numero
+},0)
