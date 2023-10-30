983f39cbf7c0c6ed6429206141a7f30455295363
zaagagon <zaagagon@gmail.com>
2023-10-30 14:24:42 -0500

    funcion scope y prueba

--- CODIFICACION/scope2.js
+++ CODIFICACION/scope2.js
@@ -2,7 +2,9 @@
 // Variable con ámbito global
 let globalVar = "Soy yo variable global";
 
-function ejemploScope() {
+//creamos funcion
+
+function pruebaScope() {
   // Variable con ámbito local
   let localVar = "Soy yo variable local";
 // Accesible: Imprimirá "Soy global"
@@ -11,7 +13,8 @@ function ejemploScope() {
   console.log(localVar);  
 }
 
-ejemploScope();
+//llamado de la funcion
+pruebaScope();
 
 console.log(globalVar); // Accesible: Imprimirá "Soy global"
 console.log(localVar);  // No accesible: Generará un error, localVar no está definida aquí
