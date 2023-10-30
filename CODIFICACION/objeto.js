6d55c68cc033f0c8eb8b19eb9e4248632d6c52be
zaagagon <zaagagon@gmail.com>
2023-10-30 17:05:50 -0500

    objeto guardado

---
+++ CODIFICACION/objeto.js
@@ -0,0 +1,12 @@
+// Crear un objeto
+const persona = {
+    nombre: "Carol",
+    apellido: "fury",
+    edad: 30,
+    ciudad: "Espacio",
+  };
+  
+  // Muestra objeto en la consola
+  console.log(persona);
+  console.log(persona.edad);
+  console.log(persona["nombre"]);
---
+++ objeto.js
@@ -0,0 +1,11 @@
+// Crear un objeto
+const persona = {
+    nombre: "Carol",
+    apellido: "fury",
+    edad: 30,
+    ciudad: "Espacio",
+  };
+  
+  // Muestra objeto en la consola
+  console.log(persona);
+  
