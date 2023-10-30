42c34311a300a3410f9b94a7a50eff0dc4fc5c09
zaagagon <zaagagon@gmail.com>
2023-10-30 16:34:37 -0500

    cuadrado ready

---
+++ CODIFICACION/cuadrado.js
@@ -0,0 +1,13 @@
+// Ingresar la longitud del lado del cuadrado
+//ladoxlado
+const lado = parseFloat(prompt("Ingresar longitud del lado del cuadrado :"));
+
+// Calcular el área
+const area = lado * lado;
+
+// Calcular el perímetro
+const perimetro = 4 * lado;
+
+// Mostrar los resultados
+console.log(`Área del cuadrado: ${area}`);
+console.log(`Perímetro del cuadrado: ${perimetro}`);
