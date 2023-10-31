39d7c1e67fef6b76ef6654bfc568286464b99423
zaagagon <zaagagon@gmail.com>
2023-10-30 22:18:28 -0500

    analizando process.argv

---
+++ CODIFICACION/scripts/args.js
@@ -0,0 +1,10 @@
+//objetos process.argv
+
+const argumentos=console.log(process.argv);
+console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+//
+
