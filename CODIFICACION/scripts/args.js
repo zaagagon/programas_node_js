e9458421d89993491d9bdbc9d7f73a7f3a96fb6a
zaagagon <zaagagon@gmail.com>
2023-10-30 22:25:00 -0500

    tamaño arreglo

--- CODIFICACION/scripts/args.js
+++ CODIFICACION/scripts/args.js
@@ -1,10 +1,11 @@
 //objetos process.argv
 
-const argumentos=console.log(process.argv);
+const argumentos=process.argv;
 console.log(process.argv[0]);
 console.log(process.argv[1]);
 console.log(process.argv[2]);
 console.log(process.argv[3]);
 
-//
+//recorremos el objeto de argv
+console.log("tamaño arreglo" +argumentos.length);
 
