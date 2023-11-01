bd847ce892ea4b5a1f4f82376a44facf11cd75ba
zaagagon <zaagagon@gmail.com>
2023-11-01 00:16:43 -0500

    nuevo arreglo

--- CODIFICACION/scripts/arreglo_push.js
+++ CODIFICACION/scripts/arreglo_push.js
@@ -7,3 +7,10 @@ frutas.forEach(fruta => {
 });
 
 //push metodo 
+frutas.push("banano")
+console.log("nuevo arreglo");
+for (let i = 0; i < frutas.length; i++) {
+    const element = frutas[i];
+    console.log(element);
+    
+}
