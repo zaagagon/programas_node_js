43365684e5f1616cc6b8214f0fd9905ebcaac9d0
zaagagon <zaagagon@gmail.com>
2023-11-01 01:04:29 -0500

    arreglo ready con metodos

--- CODIFICACION/scripts/arreglos_met.js
+++ CODIFICACION/scripts/arreglos_met.js
@@ -1,59 +1,43 @@
-// Obtiene los argumentos de línea de comandos
-const args = process.argv.slice(2);
-
-// Verifica si se proporcionaron los argumentos necesarios
-if (args.length !== 3) {
-    console.log("Uso: node programa.js fruta1 fruta2 fruta3");
-} else {
-    const frutas = args;
-
-    console.log("Arreglo de frutas inicial:", frutas);
-
-    // Muestra un menú interactivo
-    console.log("\nMenú:");
-    console.log("1. Mostrar arreglo de frutas");
-    console.log("2. Agregar una fruta");
-    console.log("3. Eliminar la última fruta");
-    console.log("4. Anexar una fruta al final");
-    console.log("5. Anexar una fruta al principio");
-
-    const readline = require('readline');
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.question("Seleccione una opción: ", function(opcion) {
-        switch (opcion) {
-            case '1':
-                console.log("Arreglo de frutas actual:", frutas);
-                break;
-            case '2':
-                rl.question("Introduce una fruta: ", function(fruta) {
-                    frutas.push(fruta);
-                    console.log("Fruta agregada. Nuevo arreglo de frutas:", frutas);
-                });
-                break;
-            case '3':
-                frutas.pop();
-                console.log("Última fruta eliminada. Nuevo arreglo de frutas:", frutas);
-                break;
-            case '4':
-                rl.question("Introduce una fruta para anexar al final: ", function(fruta) {
-                    frutas.push(fruta);
-                    console.log("Fruta anexada al final. Nuevo arreglo de frutas:", frutas);
-                });
-                break;
-            case '5':
-                rl.question("Introduce una fruta para anexar al principio: ", function(fruta) {
-                    frutas.unshift(fruta);
-                    console.log("Fruta anexada al principio. Nuevo arreglo de frutas:", frutas);
-                });
-                break;
-            default:
-                console.log("Opción no válida.");
-                break;
-        }
-        rl.close();
-    });
-}
+// Función para sumar los elementos de un arreglo
+function sumarArreglo(arreglo) {
+    let suma = 0;
+    for (let i = 2; i < arreglo.length; i++) {
+      suma += parseInt(arreglo[i]);
+    }
+    console.log("La suma de los elementos del arreglo es: " + suma);
+  }
+  
+  // Función para encontrar el elemento más grande de un arreglo
+  function encontrarMaximo(arreglo) {
+    const maximo = Math.max(...arreglo.slice(2).map(Number));
+    console.log("El elemento más grande del arreglo es: " + maximo);
+  }
+  
+  // Función para encontrar el elemento más pequeño de un arreglo
+  function encontrarMinimo(arreglo) {
+    const minimo = Math.min(...arreglo.slice(2).map(Number));
+    console.log("El elemento más pequeño del arreglo es: " + minimo);
+  }
+  
+  const args = process.argv;
+  
+  if (args.length < 3) {
+    console.log("Por favor, proporciona una opción (1/2/3) y al menos un número.");
+  } else {
+    const opcion = args[2];
+  
+    switch (opcion) {
+      case "1":
+        sumarArreglo(args);
+        break;
+      case "2":
+        encontrarMaximo(args);
+        break;
+      case "3":
+        encontrarMinimo(args);
+        break;
+      default:
+        console.log("Opción no válida. Por favor, selecciona una opción válida (1/2/3).");
+    }
+  }
+  
