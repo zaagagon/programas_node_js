ad7afc7652ec10c940912df15b551c832546668c
zaagagon <zaagagon@gmail.com>
2023-11-01 22:34:42 -0500

    hoja de estilos creada

---
+++ examples_react/mi-menu/src/components/Acerca.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Acerca() {
+  return (
+    <div>
+      <h2>Acerca de Nosotros</h2>
+      <p>Esta es la página de acerca de nosotros.</p>
+    </div>
+  );
+}
+
+export default Acerca;
