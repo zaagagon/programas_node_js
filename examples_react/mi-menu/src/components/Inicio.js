e1fb66ee6be962db3698deabd5867c2271990bd5
zaagagon <zaagagon@gmail.com>
2023-11-01 22:24:03 -0500

    componente inicio

---
+++ examples_react/mi-menu/src/components/Inicio.js
@@ -0,0 +1,13 @@
+//componente inicio
+import React from 'react';
+
+function Inicio() {
+  return (
+    <div>
+      <h2>Página de Inicio</h2>
+      <p>Bienvenido a la página de inicio.</p>
+    </div>
+  );
+}
+//exportamos componente
+export default Inicio;
