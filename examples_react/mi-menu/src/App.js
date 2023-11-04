987f1d5c690fbbf625b4cc047f52720a4262094b
zaagagon <zaagagon@gmail.com>
2023-11-03 21:31:57 -0500

    creacion de insertar datos en mysql con react

--- examples_react/mi-menu/src/App.js
+++ examples_react/mi-menu/src/App.js
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Inicio2 from './components/Inicio2';
 import Inicio from './components/Inicio';
 import Acerca from './components/Acerca';
+import InsertarDatos from './components/InsertarDatos'; // Importa el componente de inserción
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'; // Importa Axios
+import axios from 'axios';
 
 function App() {
-  // Define un estado para almacenar los datos obtenidos
   const [datos, setDatos] = useState([]);
 
-  // Función para cargar datos desde el servidor
   const cargarDatos = async () => {
     try {
-      // Utiliza Axios para realizar la solicitud
-      const response = await axios.get('/api/obtener-datos'); // Reemplaza con tu ruta de API
+      const response = await axios.get('/api/obtener-datos');
       if (response.status === 200) {
         setDatos(response.data);
       }
@@ -24,7 +22,6 @@ function App() {
   };
 
   useEffect(() => {
-    // Llama a la función para cargar datos cuando el componente se monta
     cargarDatos();
   }, []);
 
@@ -33,13 +30,15 @@ function App() {
       <div className="container mt-4">
         <nav>
           <Link to="/" className="btn btn-primary me-2">Inicio</Link>
-          <Link to="/acerca" className="btn btn-secondary">Acerca</Link>
+          <Link to="/acerca" className="btn btn-secondary me-2">Acerca</Link>
+          <Link to="/insertar-datos" className="btn btn-success">Insertar Datos</Link> {/* Agrega un enlace para la inserción */}
         </nav>
 
         <Routes>
           <Route path="/api" element={<Inicio2 />} />
           <Route path="/" element={<Inicio />} />
           <Route path="/acerca" element={<Acerca datos={datos} />} />
+          <Route path="/insertar-datos" element={<InsertarDatos />} /> {/* Agrega una nueva ruta para la inserción */}
         </Routes>
       </div>
     </Router>
