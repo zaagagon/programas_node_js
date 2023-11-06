81e1de704ad6c8a8605f2e0d0104ffeb9fe1245f
zaagagon <zaagagon@gmail.com>
2023-11-05 23:23:52 -0500

    revisando actualizar datos

--- examples_react/mi-menu/src/components/ActualizarDatos.js
+++ examples_react/mi-menu/src/components/ActualizarDatos.js
@@ -1,1 +1,59 @@
-//actalizar datos
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function ActualizarDatos() {
+  const [nombre, setNombre] = useState('');
+  const [nuevoApellido, setNuevoApellido] = useState('');
+
+  const handleNombreChange = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const handleNuevoApellidoChange = (e) => {
+    setNuevoApellido(e.target.value);
+  };
+
+  const handleActualizarClick = async () => {
+    try {
+      // Realiza una solicitud al servidor para actualizar los datos (usando PUT)
+      const response = await axios.put(`/api/actualizar-datos/${nombre}`, {
+        nuevoApellido: nuevoApellido,
+      });
+
+      if (response.status === 200) {
+        alert('Datos actualizados correctamente');
+      } else {
+        alert('Error al actualizar los datos');
+      }
+    } catch (error) {
+      console.error('Error al actualizar datos:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Actualizar Datos</h2>
+      <div className="form-group">
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+          type="text"
+          id="nombre"
+          value={nombre}
+          onChange={handleNombreChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="nuevoApellido">Nuevo Apellido:</label>
+        <input
+          type="text"
+          id="nuevoApellido"
+          value={nuevoApellido}
+          onChange={handleNuevoApellidoChange}
+        />
+      </div>
+      <button onClick={handleActualizarClick}>Actualizar Datos</button>
+    </div>
+  );
+}
+
+export default ActualizarDatos;
