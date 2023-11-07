0c4044071cc0fb203fa16ca2d423a16de130ad48
zaagagon <zaagagon@gmail.com>
2023-11-07 00:08:24 -0500

    arreglo con map nuevo

--- CODIFICACION/array2.js
+++ CODIFICACION/array2.js
@@ -1,9 +1,17 @@
 const numeros=[3,4]
 
+console.log(numeros);
+
 console.log(numeros.length);
 
 console.log(numeros.push(7));
 
 console.log(numeros[2]);
 
+//creamos nuevo arreglo
 
+let arreglo_nuevo=numeros.map(function(elemento){
+    return elemento;
+})
+
+console.log(arreglo_nuevo); 
