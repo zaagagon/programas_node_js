22686f07d61b967cc67243c0919428f946b386fa
zaagagon <zaagagon@gmail.com>
2023-11-07 00:12:29 -0500

    recorrido arreglo foreach

--- CODIFICACION/array2.js
+++ CODIFICACION/array2.js
@@ -1,9 +1,9 @@
 const numeros=[3,4]
-
+//arreglo original 
 console.log(numeros);
-
+//tamaño del arreglo
 console.log(numeros.length);
-
+//insertar elemento al final del arreglo
 console.log(numeros.push(7));
 
 console.log(numeros[2]);
@@ -15,3 +15,10 @@ let arreglo_nuevo=numeros.map(function(elemento){
 })
 
 console.log(arreglo_nuevo); 
+
+//recorrido del arreglo
+console.log("recorrido arreglo con foreach");
+numeros.forEach(element => {
+    console.log(element); 
+    
+});
