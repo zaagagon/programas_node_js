3178acba38b11f8eee35bfa6eee1c550393f7b5e
zaagagon <zaagagon@gmail.com>
2023-11-09 07:34:18 -0500

    desarrollo activo php

--- examples_react/mi-menu/backend/server.js
+++ examples_react/mi-menu/backend/server.js
@@ -31,7 +31,7 @@ db.connect(err => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
   } else {
-    console.log('Conexión a la base de datos exitosa');
+    console.log('Conexión a la base de datos exitosa, ok');
   }
 });
 
--- examples_react/mi-menu/src/components/Acerca.js
+++ examples_react/mi-menu/src/components/Acerca.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Acerca() {
   return (
     <div>
+      <h1>Pagina en construcción</h1>
       <h2>Acerca de Nosotros</h2>
       <p>Esta es la página de acerca de nosotros.</p>
     </div>
