04781f2982318320bac52454f5cc425692ffebed
zaagagon <zaagagon@gmail.com>
2023-11-13 13:10:11 -0500

    revisar objeto y arreglo heroes

--- CODIFICACION/scripts/dato_argv.js
+++ CODIFICACION/scripts/dato_argv.js
@@ -10,24 +10,26 @@ let ironMan = {
     nombre: 'Tony',
     nick: 'Iron Man'
 };
-
+//let heroes = [capitanAmerica, ironMan, blackWidow, thor];
+let heroes = [capitanAmerica, ironMan];
 //organizar en un array
 heroes.forEach((heroe, indice) => {
     console.log(`\nInformación del héroe ${indice + 1}:`);
-    console.log('Nombre:', heroe.nombre);
-    console.log('Nick:', heroe.nick);
+    console.log('Nombre:', heroes.nombre);
+    console.log('Nick:', heroes.nick);
 });
 
 //muestra información del objeto
-console.log(heroe.nombre);
+console.log(heroes.nombre);
 
 
 //object keys
 //clave : valor
-let keys= Object.keys(heroe)
+
+let keys= Object.keys(heroes)
 //muestra información del objeto
 keys.forEach(function(key) {
-    console.log(key + ': ' + heroe[key]);
+    console.log(key + ': ' + heroes[key]);
   });
 
   let tamaño=Object.keys.length
