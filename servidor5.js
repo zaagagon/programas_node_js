7b79d4e87577006e2e7261a9cb7946b876ff9355
zaagagon <zaagagon@gmail.com>
2023-11-18 00:09:00 -0500

    backtricks puerto

--- servidor5.js
+++ servidor5.js
@@ -6,4 +6,5 @@ const app= express()
 let puerto = process.argv[2]
 //escucha 5001
 app.listen(puerto)
-console.log("servidor ok en puerto 5001");
+//console.log("servidor ok en puerto 5001");
+console.log(`servidor funcionando en ${puerto}`);
