a7e2561137d2249d6190a3dd5fa139dd5292c842
zaagagon <zaagagon@gmail.com>
2023-11-29 23:21:48 -0500

    objetos en js

---
+++ CODIFICACION/objeto2.js
@@ -0,0 +1,12 @@
+// Crear un objeto
+var persona = {
+    nombre: 'Juan',
+    edad: 25,
+    ciudad: 'Ejemploville'
+  };
+  
+  // Mostrar propiedades del objeto en la consola
+  console.log("Nombre: " + persona.nombre);
+  console.log("Edad: " + persona.edad);
+  console.log("Ciudad: " + persona.ciudad);
+  
