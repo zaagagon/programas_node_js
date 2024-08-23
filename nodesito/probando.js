b1631aaaa8b8ac91eb28cc01895b1d2c2dca7a0e
zaagagon <zaagagon@gmail.com>
2024-08-23 18:18:19 -0500

    probando git en js

--- nodesito/probando.js
+++ nodesito/probando.js
@@ -1,9 +1,11 @@
 // Definimos dos números para sumar
-const num1 = 5;
-const num2 = 10;
+const num1 = 98;
+const num2 = 105;
 
 // Sumamos los dos números
 const resultado = num1 + num2;
 
 // Mostramos el resultado por consola
+console.log("Practicando Git ...");
+
 console.log("La suma de", num1, "y", num2, "es:", resultado);
