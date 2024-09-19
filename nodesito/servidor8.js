14b4e4fb2cdb5444b00ea544657db9f0086ee2b2
zaagagon <zaagagon@gmail.com>
2024-09-19 14:48:33 -0500

    servidor 8

---
+++ nodesito/servidor8.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3010
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'))
+
+})
+
+app.get('/mensaje', (req, res) => {
+  res.send('Hola CodeExplorer desde Express')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
