87553830252b59f5596d8af5e46cb9ff138d9ee7
zaagagon <zaagagon@gmail.com>
2024-09-19 13:08:12 -0500

    cambios en servidor7 backend express con json

--- nodesito/servidor7.js
+++ nodesito/servidor7.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => res.send('<h1>Bienvenido a mi API!<h1/>'))
 app.get('/api/products', (req, res) => {
     const products= [
         {id:1,name :'Logan',nickname:'wolverine' },
-        {id:2,name :'Logan',nickname:'wolverine' }
+        {id:2,name :'Wade',nickname:'Deadpool' }
 
     ]
   res.send(products)
