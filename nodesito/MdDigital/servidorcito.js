6d8127bb42759212ec121c2edbe4baa51ab6db92
zaagagon <zaagagon@gmail.com>
2024-09-19 21:28:28 -0500

    nuevo servidor9

--- nodesito/MdDigital/servidorcito.js
+++ nodesito/MdDigital/servidorcito.js
@@ -4,7 +4,10 @@ const app = express()
 
 const port=3003
 
-//definir ruta
+//definir ruta para solicitudes http de tipo get
+app.get('/',(req,res) => {
+    res.send('hola desde express en el puerto 3003')
+})
 
 
 app.listen(port,() => 
