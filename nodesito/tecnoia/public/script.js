a30d960ef729377f82d80b0b6c5e59b7c40822e6
zaagagon <zaagagon@gmail.com>
2024-09-20 00:40:35 -0500

    practicando express

---
+++ nodesito/tecnoia/public/script.js
@@ -0,0 +1,11 @@
+//mostrar una alerta  cuando la pagina cargue
+window.onload=function () {
+    alert('Bienvenidos a Express CodeExplorers')
+}
+
+//funcion para cambiar color
+function cambiarColor() {
+    const titulo =document.querySelector('h1')
+    titulo.style.color='blue'
+    
+}
---
+++ nodesito/tecnoia/servidor.js
@@ -0,0 +1,44 @@
+//importando express
+const express=require('express')
+const path=require('path')
+//instancia de express
+const app=express()
+//puerto del servidor
+const port=process.env.port || 3001
+
+//middleware servir archivo estaticos desde la carpeta public
+app.use(express.static(path.join(__dirname,'public')))
+
+//ruta de inicio 
+app.get('/inicio', (req, res) => {
+  res.sendFile(path.join(__dirname,'public','menu.html'))
+})
+app.get('/accesorios', (req, res) => {
+  res.sendFile(path.join(__dirname,'public','accesorios.html'))
+})
+app.get('/clientes', (req, res) => {
+  res.sendFile(path.join(__dirname,'public','clientes.html'))
+})
+
+app.get('/prueba', (req, res) => {
+  res.send('<h1 style="color:blue">Hola Bienvenidos CodeExplorers desde Express**</h1>')
+})
+
+//ruta api de personajes
+app.get('/api/personajes', (req, res) => {
+    const personajes=[
+        {id:1,nombre:'logan',nickname:'wolverine'},
+        {id:2,nombre:'wade',nickname:'deadpool'},
+        
+    ]
+  res.json(personajes)
+})
+
+app.get('/mensaje', (req, res) => {
+  res.send('<h2 style="color:red">**Hola CodeExplorers desde ruta mensaje **</h2>')
+})
+
+app.listen(port,()=>{
+    console.log(`Servidor escuchando en el puerto ${port}`);
+    
+})
