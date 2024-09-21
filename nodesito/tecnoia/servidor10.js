8aafa6388d8e7ffaa8813cd56c4948da05a85670
zaagagon <zaagagon@gmail.com>
2024-09-20 21:11:04 -0500

    middleware con express static server 10

--- nodesito/tecnoia/servidor10.js
+++ nodesito/tecnoia/servidor10.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const path=require('path')
 const port = 3000
+//middleware para servir archivos estaticos desde carpeta publica
+app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req, res) => res.send('Hello Code!'))
+//ruta de arranque
+app.get('/', (req, res) => res.send('Hello CodeExplorers!'))
+
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
