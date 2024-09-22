cc35389072dfbfd4c2f655bac197fcecb7a9b9e7
zaagagon <zaagagon@gmail.com>
2024-09-21 23:58:55 -0500

    creando datos personajes con json

--- nodesito/tecnoia/servidor10.js
+++ nodesito/tecnoia/servidor10.js
@@ -5,7 +5,20 @@ const port = 3000
 //middleware para servir archivos estaticos desde carpeta publica
 app.use(express.static(path.join(__dirname,'public')))
 
-//ruta de arr
+//ruta de mensaje
 app.get('/mensaje', (req, res) => res.send('Hello CodeExplorers **!'))
+
+app.get('/', (req, res) => {
+    const personajes=[
+
+        {
+            name:'logan',
+            nickname:'wolverine',
+            power: 'healing factor, adamantium claws'
+        },
+        {}
+    ]
+  res.send('GET request to the homepage')
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
