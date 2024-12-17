9f0cefb2974d9007a409cd66b411770d6e1cfb3b
zaagagon <zaagagon@gmail.com>
2024-12-17 00:01:55 -0500

    corriegiendo codigo con module.exports para suma en js

--- CODIFICACION/suma.js
+++ CODIFICACION/suma.js
@@ -6,7 +6,7 @@ const num2 = parseFloat(process.argv[3]);
 
 if (!isNaN(num1) && !isNaN(num2)) {
   const suma = num1 + num2;
-  console.log(`La suma de ${num1} y ${num2} es: ${suma}`);
+  console.log(`La suma es funcion de ${num1} y ${num2} es: ${suma}`);
 } else {
   console.log('Por favor, ingrese dos números válidos.');
 }
--- CODIFICACION/sumita_arg.js
+++ CODIFICACION/sumita_arg.js
@@ -1,3 +1,5 @@
+import {suma} from "./supersuma.js"
+//const resu=supersuma()
 //programa que suma dos numeros ingresados al ejecutar
 n1=process.argv[2]
 n2 =process.argv[3]
@@ -6,5 +8,6 @@ const num2= parseFloat(n2)
 console.log(n1);
 console.log(n2);
 console.log(num1+num2);
+suma(1,2)
 
 //n1=process.argv[2]
