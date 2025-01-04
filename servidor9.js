e77842f50c9bdc9ec106ffa64d2f27c911d572c8
zaagagon <zaagagon@gmail.com>
2025-01-03 19:13:51 -0500

    cambios en servidor con public

---
+++ servidor9.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Configurar carpeta pública para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para home.html
+app.get('/hola', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'hola.html'));
+});
+
+// Ruta para about.html
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+// Iniciar servidor
+const PORT = 9011;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
